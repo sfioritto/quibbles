37fa6d43aa92a132b42e2e879970fa682fcb5046
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-08-14 12:55:58 -0500

    Added a test.

--- tests/email/handlers/talking_tests.py
+++ tests/email/handlers/talking_tests.py
@@ -5,6 +5,7 @@ from lamson.routing import Router
 from lamson.mail import MailRequest
 from lamson import queue
 from conf import *
+from webapp.talking.models import Answer
 
 
 relay = relay(port=8823)
@@ -13,6 +14,7 @@ sender = "test@localhost"
 
 
 def setup_func():
+    Answer.objects.all().delete()
     q = queue.Queue(email('run/work'))
     q.clear()
 
@@ -31,4 +33,21 @@ def test_talking():
     Router.deliver(msg)
     q = queue.Queue(email('run/work'))
     assert q.count() == 2, "Queue count is actually %s" % str(q.count())
+
+
+@with_setup(setup_func, teardown_func)
+def test_get_work():
+
+    """
+    This message should move the state into
+    TALKING.
+    """
+    # add some work to the work queue
+    test_talking()
+    msg = MailRequest('fakepeer', 'new@sender.com', "talk@mr.quibbl.es", open(home("tests/data/emails/question.msg")).read())
+    msg['From'] = 'new@sender.com'
+    Router.deliver(msg)
+    q = queue.Queue(email('run/work'))
+    assert q.count() == 2, "Queue count is actually %s" % str(q.count())
+    assert len(Answer.objects.all()) == 4, "Oops. There are actually %s answers in the db, expected 4." % str(len(Answer.objects.all()))
 
